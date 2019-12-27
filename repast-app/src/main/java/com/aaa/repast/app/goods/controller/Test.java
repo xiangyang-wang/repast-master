b05861ae414a34afee6cd3d6ec603ac0e0a2e3d2
xiangyang-wang <1356161332@qq.com>
2019-12-27 19:56:37 +0800

    后台项目初始化

---
+++ repast-app/src/main/java/com/aaa/repast/app/goods/controller/Test.java
@@ -0,0 +1,17 @@
+package com.aaa.repast.app.goods.controller;
+
+/**
+ * @Company AAA软件教育
+ * @Author Seven Lee
+ * @Date Create in 2019/12/4 13:00
+ * @Description
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++;
+        System.out.println(i);
+    }
+
+}
